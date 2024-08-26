b0e3b32725f26fa8f2c62d58cec873fe185ece75
Josh <canterrain@live.com>
2024-08-26 15:05:48 -0400

    Update generateToken.py
    adding playlist functionality

--- python/generateToken.py
+++ python/generateToken.py
@@ -1,15 +1,21 @@
 import sys
 from spotipy.oauth2 import SpotifyOAuth
 
-
 def main():
     if len(sys.argv) > 1:
         username = sys.argv[1]
-        scope = 'user-read-currently-playing,user-modify-playback-state'
+        
+        # Expanded scope to include playlist-related permissions
+        scope = (
+            'user-read-currently-playing,user-modify-playback-state,'
+            'playlist-read-private,playlist-read-collaborative,'
+            'playlist-modify-public,playlist-modify-private'
+        )
 
         # This way removes the need for a browser, it will instead give the URL to visit in the terminal
         auth = SpotifyOAuth(scope=scope, open_browser=False)
         token = auth.get_access_token(as_dict=False)
+        
         if not token:
             print('Error unable to get token', file=sys.stderr)
             sys.exit(-1)
